c333165bf7725810c9128f67a9a7d0eada9877f8
mapumarshmallow <mapu.park@gmail.com>
2019-09-28 20:04:48 -0500

    home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { MinistryComponent } from './ministry/ministry.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 const routes: Routes = [
+  {path: 'home', component: HomepageComponent},
   {path: 'contacts' , component: ContactComponent},
   {path: 'ministries' , component: MinistryComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TitleComponent } from './home-page/title/title.component';
-import { HeaderComponent } from './home-page/header/header.component';
-import { BodyComponent } from './home-page/body/body.component';
+import { TitleComponent } from './homepage/title/title.component';
+import { HeaderComponent } from './homepage/header/header.component';
+import { BodyComponent } from './homepage/body/body.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactComponent } from './contact/contact.component';
 import { MinistryComponent } from './ministry/ministry.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { MinistryComponent } from './ministry/ministry.component';
     BodyComponent,
     NavBarComponent,
     ContactComponent,
-    MinistryComponent
+    MinistryComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
